17541c108c6f1471acb3f51f9e0dc6f1366ed962
Julius Seiffert <julius.seiffert@stud.htwk-leipzig.de>
2019-08-05 16:09:48 +0200

    Initialize project

---
+++ Project_T/src/main/java/com/htwk/jseiffer/App.java
@@ -0,0 +1,13 @@
+package com.htwk.jseiffer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
