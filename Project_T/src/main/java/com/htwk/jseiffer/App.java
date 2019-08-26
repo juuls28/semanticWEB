de21cce6bd01fa78cbe41c11426a9c8f82ee8ff4
jseiffer <julius.seiffert@stud.htwk-leipzig.de>
2019-08-26 15:57:45 +0200

    Finished SPARQL Query

--- Project_T/src/main/java/com/htwk/jseiffer/App.java
+++ Project_T/src/main/java/com/htwk/jseiffer/App.java
@@ -7,32 +7,71 @@ import com.htwk.jseiffer.terror.TerrorCrawler;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class App 
 {
     public static void main( String[] args )
     {
-        LocalDate start = LocalDate.parse("2015-07-28");
-        LocalDate end = LocalDate.parse("2018-07-28");
+        App app = new App();
+
+        String startDate = "2016-02-02";
+        String endDate = "2017-02-02";
+
+        String attackDate = "";
+
+        String firstPollDate;
+        String secPollDate;
+
+        String firstStockDate;
+        String secStockDate;
+
+        HashMap<Integer, String> attacks = new HashMap<>();
+
+        int attackNumber;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("Willkommen zur Terrorauswertung.\n" +
+                "Bitte geben Sie einen Zeitraum an, aus dem Sie Anschläge auswählen können." +
+                "Ab welchen Datum? (yyyy-MM-dd)\n");
+        startDate = scan.nextLine();
+        System.out.printf("Bis zu welchem Datum? (yyyy-MM-dd)\n");
+        endDate = scan.nextLine();
+
+
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
         StockCrawler stockCrawler = new StockCrawler(start, end);
-        TerrorCrawler terror = new TerrorCrawler(2008,2017);
+        TerrorCrawler terror = new TerrorCrawler(start.getYear(),end.getYear());
         ModelBuilder builder = new ModelBuilder(terror.getAttacks(), stockCrawler.getStocks());
         builder.createModel();
-        builder.writeModel();
         builder.saveModel();
 
         Model model = builder.getModel();
 
+        // Starting querying the data
+        String queryString;
+        Query query;
 
-
-        String queryString = "SELECT ?s  " +
-                "WHERE { ?s <http://www.terror.org#happenedOn> \"2015-09-19\"  }" ;
+        //Let the user see the attacks in given time interval
+        queryString = "Prefix ns: <https://github.com/juuls28/semanticWEB#>\n" +
+                "SELECT ?s ?c ?d ?o\n" +
+                             "WHERE { ?s ns:hasInjuries ?o . \n" +
+                "?s ns:inCity ?c .\n" +
+                "?s ns:happenedOn ?d .\n" +
+                "}\n" +
+                "ORDER BY ASC(?d)" ;
 
         System.out.println("Query: " + queryString);
-        Query query = QueryFactory.create(queryString) ;
+        query = QueryFactory.create(queryString) ;
         try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
             ResultSet results = qexec.execSelect() ;
 
@@ -40,11 +79,94 @@ public class App
             {
                 System.out.println("Number: " + results.getRowNumber());
                 QuerySolution soln = results.nextSolution() ;
-                RDFNode x = soln.get("varName") ;       // Get a result variable by name.
-                Resource r = soln.getResource("VarR") ; // Get a result variable - must be a resource
-                Literal l = soln.getLiteral("VarL") ;   // Get a result variable - must be a literal
-                System.out.println(soln.toString());
+                System.out.println("Id: " + soln.get("?s") + "\tDate: " + soln.get("?d") + "\tCity: " + soln.get("?c"));
+                //add attack to map
+                attacks.put(results.getRowNumber(), soln.get("?s").toString());
             }
+        }
+
+        //Ask User for the attack
+        System.out.println("Welcher Terrorangriff soll ausgewertet werden? (Tippe eine Zahl)");
+        attackNumber = scan.nextInt();
+
+        //Query the date for the attack again
+        queryString = "Prefix ns: <https://github.com/juuls28/semanticWEB#>\n" +
+                "SELECT ?d\n" +
+                "WHERE { <" + attacks.get(attackNumber) + "> ns:happenedOn ?d . \n" +
+                "}" ;
+
+        System.out.println("Query: " + queryString);
+        query = QueryFactory.create(queryString) ;
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect() ;
+
+            for ( ; results.hasNext() ; )
+            {
+                QuerySolution soln = results.nextSolution() ;
+                System.out.println("Date: " + soln.get("?d"));
+                attackDate = soln.get("?d").toString();
+            }
+        }
+
+        queryString = "Prefix ns: <https://github.com/juuls28/semanticWEB#>\n" +
+                "\n" +
+                "SELECT ?firstStock ?secondStock ?vF ?vS ?firstPoll ?secondPoll ?p1Name ?p1Percent ?p2Name ?p2Percent\n" +
+                "WHERE { \n" +
+                "  {Select ?firstStock\n" +
+                "    WHERE{\n" +
+                "      ?firstStock ns:happenedOn ?date .\n" +
+                "      ?firstStock ns:value ?o .\n" +
+                "      Filter(?date < \"" + attackDate + "\")\n" +
+                "    } Order By DESC (?date)\n" +
+                "    Limit 1\n" +
+                "  }\n" +
+                "  {Select ?secondStock\n" +
+                "    WHERE{\n" +
+                "      ?secondStock ns:happenedOn ?date .\n" +
+                "      ?secondStock ns:value ?o .\n" +
+                "      Filter(?date > \"" + attackDate + "\")\n" +
+                "    } Order By ASC (?date)\n" +
+                "    Limit 1\n" +
+                "  }\n" +
+                "  ?firstStock ns:value ?vF .\n" +
+                "  ?secondStock ns:value ?vS .\n" +
+                "  {Select ?firstPoll\n" +
+                "    WHERE{\n" +
+                "      ?firstPoll ns:happenedOn ?date .\n" +
+                "      ?firstPoll ns:outcomes ?o .\n" +
+                "      Filter(?date < \"" + attackDate + "\")\n" +
+                "    } Order By DESC (?date)\n" +
+                "    Limit 1\n" +
+                "  }\n" +
+                "  {Select ?secondPoll\n" +
+                "    WHERE{\n" +
+                "      ?secondPoll ns:happenedOn ?date .\n" +
+                "      ?secondPoll ns:outcomes ?o .\n" +
+                "      Filter(?date > \"" + attackDate + "\")\n" +
+                "    } Order By ASC (?date)\n" +
+                "    Limit 1\n" +
+                "  }\n" +
+                "  ?firstPoll ns:outcomes ?outF .\n" +
+                "  ?outF ns:partyName ?p1Name .\n" +
+                "  ?outF ns:partyPercent ?p1Percent .\n" +
+                "  ?secondPoll ns:outcomes ?outS .\n" +
+                "  ?outS ns:partyName ?p2Name .\n" +
+                "  ?outS ns:partyPercent ?p2Percent .\n" +
+                "  Filter(?p1Name = ?p2Name) \n" +
+                "}";
+
+        System.out.println("Query: " + queryString);
+        query = QueryFactory.create(queryString) ;
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect() ;
+
+            for ( ; results.hasNext() ; )
+            {
+                System.out.println("Number: " + results.getRowNumber());
+                QuerySolution soln = results.nextSolution() ;
+                System.out.println(soln);
+            }
+
         }
     }
 }
--- Project_T/src/main/java/com/htwk/jseiffer/ModelBuilder.java
+++ Project_T/src/main/java/com/htwk/jseiffer/ModelBuilder.java
@@ -6,14 +6,14 @@ import com.htwk.jseiffer.poll.Party;
 import com.htwk.jseiffer.poll.Poll;
 import com.htwk.jseiffer.poll.PollCrawler;
 import com.htwk.jseiffer.terror.Terror;
+import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.XSD;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +23,8 @@ public class ModelBuilder {
     private Model model;
     private List<Terror> attacks;
     private List<Stock> stocks;
+
+    private String ns = "https://github.com/juuls28/semanticWEB#";
 
     public ModelBuilder(List<Terror> attacks, List<Stock> stocks) {
         this.attacks = attacks;
@@ -39,8 +41,6 @@ public class ModelBuilder {
     }
 
     private void createTerrorModel(){
-        String domain = "http://www.example-terror.org";
-        String ns = "http://www.terror.org#";
 
         Property date = model.createProperty(ns,"happenedOn");
         Property city = model.createProperty(ns,"inCity");
@@ -49,7 +49,7 @@ public class ModelBuilder {
         Property injuries = model.createProperty(ns, "hasInjuries");
 
         for (Terror t : attacks) {
-            Resource res = model.createResource(domain + "/terror/" + t.getId());
+            Resource res = model.createResource(ns + t.getId());
 
             res.addProperty(date,t.getDate().toString());
             res.addProperty(country,t.getCountry());
@@ -57,35 +57,24 @@ public class ModelBuilder {
             res.addProperty(fatalities, String.valueOf(t.getFatalities()));
             res.addProperty(injuries, String.valueOf(t.getInjured()));
 
-//            addStatement(domain+"/terror/"+t.getId(),ns+"inCity",t.getCity());
-//            addStatement(domain+"/terror/"+t.getId(),ns+"inCountry",t.getCountry());
-//            addStatement(domain+"/terror/"+t.getId(),ns+"happenedOn",t.getDate().toString());
-//            addStatement(domain+"/terror/"+t.getId(),ns+"hasFatalities",String.valueOf(t.getFatalities()));
-//            addStatement(domain+"/terror/"+t.getId(),ns+"hasInjured",String.valueOf(t.getInjured()));
         }
     }
 
     private void createStockModel(){
-        String domain = "http://www.example-stock.org";
-        String ns = "http://www.stock.org#";
 
         Property date = model.createProperty(ns,"happenedOn");
         Property value = model.createProperty(ns,"value");
 
         for (Stock s : stocks) {
-            Resource res = model.createResource(domain+"/stock/"+s.getId());
+            Resource res = model.createResource(ns + s.getId());
 
             res.addProperty(date, s.getDate().toString());
             res.addProperty(value, String.valueOf(s.getValue()));
 
-            //addStatement(domain+"/stock/"+s.getId(),ns+"happenedOn",s.getDate().toString());
-            //addStatement(domain+"/stock/"+s.getId(),ns+"value",String.valueOf(s.getValue()));
         }
     }
 
     private void createPollModel(HashMap<LocalDate, Poll> map){
-        String domain = "http://www.example-poll.org";
-        String ns = "http://www.poll.org#";
 
         Property date = model.createProperty(ns,"happenedOn");
         Property partyName = model.createProperty(ns,"partyName");
@@ -94,11 +83,11 @@ public class ModelBuilder {
 
 
         for (Map.Entry<LocalDate, Poll> entry: map.entrySet()) {
-            Resource poll = model.createResource(domain+"/poll/"+entry.getKey().toString());
+            Resource poll = model.createResource(ns + entry.getKey().toString());
             poll.addProperty(date, String.valueOf(entry.getKey()));
 
             for(Party p : entry.getValue().getOutcomes()){
-                Resource party = model.createResource(domain+"/party/"+entry.getKey().toString()+p.getName());
+                Resource party = model.createResource(ns + entry.getKey().toString()+p.getName());
 
                 party.addProperty(partyName, p.getName());
                 party.addProperty(partyPercent, String.valueOf(p.getPercent()));
@@ -114,9 +103,13 @@ public class ModelBuilder {
     public void createModel(){
         PollCrawler polls = new PollCrawler();
         model = ModelFactory.createDefaultModel();
+
+        model.setNsPrefix("ns",ns);
         this.createTerrorModel();
         this.createPollModel(polls.getPolls());
         this.createStockModel();
+
+        //this.createOWL(polls.getPolls());
 
 
     }
@@ -127,7 +120,7 @@ public class ModelBuilder {
 
         try {
 
-            file = new File("data\\model.txt");
+            file = new File("data\\model.rdf");
             fop = new FileOutputStream(file);
 
             // if file doesnt exists, then create it
@@ -145,4 +138,108 @@ public class ModelBuilder {
     public Model getModel() {
         return model;
     }
+
+//    private void createOWL(HashMap<LocalDate, Poll> map){
+//        String se = "https://github.com/juuls28/semanticWEB#";
+//        String xsd = "http://www.w3.org/2001/XMLSchema#";
+//        String owl = "http://www.w3.org/2002/07/owl#";
+//        String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+//        String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
+//
+//        model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+//
+//        model.setNsPrefix("se", se);
+//        model.setNsPrefix("xsd", xsd);
+//        model.setNsPrefix("owl", owl);
+//        model.setNsPrefix("rdf", rdf);
+//        model.setNsPrefix("rdfs", rdfs);
+//
+//        OntClass terror = model.createClass(se + "terror");
+//        OntClass poll = model.createClass(se + "poll");
+//        OntClass stock = model.createClass(se + "stock");
+//        OntClass party = model.createClass(se + "party");
+//
+//        DatatypeProperty date = model.createDatatypeProperty(se + "date");
+//        date.addDomain(terror);
+//        date.addDomain(poll);
+//        date.addDomain(stock);
+//        date.addRange(XSD.date);
+//
+//        DatatypeProperty city = model.createDatatypeProperty(se + "city");
+//        city.addDomain(terror);
+//        city.addRange(XSD.xstring);
+//
+//        DatatypeProperty country = model.createDatatypeProperty(se + "city");
+//        country.addDomain(terror);
+//        country.addRange(XSD.xstring);
+//
+//        DatatypeProperty fatalities = model.createDatatypeProperty(se + "city");
+//        fatalities.addDomain(terror);
+//        fatalities.addRange(XSD.xint);
+//
+//        DatatypeProperty injuries = model.createDatatypeProperty(se + "city");
+//        injuries.addDomain(terror);
+//        injuries.addRange(XSD.xint);
+//
+//        DatatypeProperty outcomes = model.createDatatypeProperty(se + "outcomes");
+//        outcomes.addDomain(poll);
+//        outcomes.addRange(party);
+//
+//        DatatypeProperty partyName = model.createDatatypeProperty(se + "partyName");
+//        partyName.addDomain(party);
+//        partyName.addRange(XSD.xstring);
+//
+//        DatatypeProperty partyPercent = model.createDatatypeProperty(se + "partyPercent");
+//        partyPercent.addDomain(party);
+//        partyPercent.addRange(XSD.xfloat);
+//
+//        DatatypeProperty value = model.createDatatypeProperty(se + "value");
+//        value.addDomain(stock);
+//        value.addRange(XSD.xfloat);
+//
+//        System.out.println("Starting with polls");
+//        for (Map.Entry<LocalDate, Poll> entry: map.entrySet()) {
+//            Individual pollIndi = model.createIndividual(se + entry.getKey().toString(),poll);
+//            poll.addProperty(date, entry.getKey().toString());
+//
+//            for(Party p : entry.getValue().getOutcomes()){
+//                Individual partyIndi = model.createIndividual(se + entry.getKey().toString()+p.getName(),party);
+//
+//                party.addProperty(partyName, p.getName());
+//                party.addProperty(partyPercent, String.valueOf(p.getPercent()));
+//
+//                poll.addProperty(outcomes, party);
+//
+//            }
+//
+//        }
+//        System.out.println("Starting with stocks");
+//        for (Stock s : stocks) {
+//            Individual stockIndi = model.createIndividual( se + s.getId(), stock);
+//
+//            stockIndi.addProperty(date, s.getDate().toString());
+//            stockIndi.addProperty(value, String.valueOf(s.getValue()));
+//        }
+//        System.out.println("Starting with terror");
+//        for (Terror t : attacks) {
+//            Individual terrorIndi = model.createIndividual(se + t.getId(),terror);
+//
+//            terrorIndi.addProperty(date,t.getDate().toString());
+//            terrorIndi.addProperty(country,t.getCountry());
+//            terrorIndi.addProperty(city,t.getCity());
+//            terrorIndi.addProperty(fatalities, String.valueOf(t.getFatalities()));
+//            terrorIndi.addProperty(injuries, String.valueOf(t.getInjured()));
+//
+//        }
+//
+//        System.out.println("Model built");
+//
+//        try {
+//            model.write(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("data\\ontology.rdf"), "UTF8")));
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
 }
